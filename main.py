efa7d64e934c0d28d5d901fd4ecc308180e0207e
Ubuntu <ubuntu@ip-172-31-2-209.us-west-2.compute.internal>
2022-11-23 05:31:48 +0000

    aws branch

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import importlib
 from lib import dataset
 from lib.config import config, update_config, infer_exp_id
 
+import wandb 
+
 
 def evaluate(pred, trg):
     size = len(pred)
@@ -91,11 +93,24 @@ def valid_loop(model, loader, loss_func):
 
 if __name__ == '__main__':
 
+    wandb.init(project="final-project", entity="cs230-virufy")
+
     # Parse args & configs
     parser = argparse.ArgumentParser()
     parser.add_argument("--cfg", help="where is the config yaml file for this run")
     args = parser.parse_args()
     update_config(config, args)
+    
+
+
+    wandb.config.update({
+            "learning_rate":config.training.lr,
+            "batch_size":config.training.batch_size,
+            "epoch": config.training.epoch,
+            "optim": config.training.optim,
+            "model": config.model.modelclass,
+            })
+    
 
     # init variable
     exp_dir, exp_id = infer_exp_id(args.cfg, config.ckpt_root)
@@ -113,19 +128,19 @@ if __name__ == '__main__':
     ## parameters passed from config.py
     DatasetClass = getattr(dataset, config.dataset.name)
     config.dataset.train_kwargs.update(config.dataset.common_kwargs)
-    config.dataset.valid_kwargs.update(config.dataset.common_kwargs)
+    #config.dataset.valid_kwargs.update(config.dataset.common_kwargs)
     config.dataset.test_kwargs.update(config.dataset.common_kwargs)
     ## parameters updated from .yaml  
     train_dataset = DatasetClass(**config.dataset.train_kwargs)
-    valid_dataset = DatasetClass(**config.dataset.valid_kwargs)
+    #valid_dataset = DatasetClass(**config.dataset.valid_kwargs)
     test_dataset = DatasetClass(**config.dataset.test_kwargs)
 
     # init dataloader
     train_loader = DataLoader(train_dataset, config.training.batch_size,
                                 shuffle=True, num_workers=config.num_workers,
                                 pin_memory=config.cuda)
-    valid_loader = DataLoader(valid_dataset, 1, num_workers=config.num_workers,
-                                pin_memory=config.cuda)
+    #valid_loader = DataLoader(valid_dataset, 1, num_workers=config.num_workers,
+                                #pin_memory=config.cuda)
     test_loader = DataLoader(test_dataset, 1, num_workers=config.num_workers,
                                 pin_memory=config.cuda)
 
@@ -150,11 +165,18 @@ if __name__ == '__main__':
               ' \ '.join([f'{k} {v:.3f}' for k, v in ep_loss.items()]))
 
         # validating
-        ep_loss = valid_loop(net, valid_loader, config.model.loss_func)
-        print(f'EP[{iep}/{config.training.epoch}] valid:  ' +
-              ' \ '.join([f'{k} {v:.3f}' for k, v in ep_loss.items()]))
+        #ep_loss = valid_loop(net, valid_loader, config.model.loss_func)
+        #print(f'EP[{iep}/{config.training.epoch}] valid:  ' +
+        #      ' \ '.join([f'{k} {v:.3f}' for k, v in ep_loss.items()]))
 
         # store the model 
+        
+        wandb.log({"train_loss": ep_loss['ave_mean'],
+            "acc":ep_loss['acc'],
+            "precision": ep_loss['precision'],
+            "recall":ep_loss['recall']})
+
+
         if (iep+1) > 0 and (iep+1)% config.training.save_every == 0:
             pth_name = config.model.modelclass +'/'+ f'ep{iep+1}.pth'
             torch.save(net.state_dict(), os.path.join(config.ckpt_root, pth_name))
